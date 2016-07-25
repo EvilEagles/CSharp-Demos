78e44a0460e14b6acfc52b85c617fb28fe73abd3
Alex Vu <alex@igv.cafe>
2016-07-25 20:24:47 +0700

    Adding all my C# projects

---
+++ DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int i, j;
+            Console.WriteLine("Please input n:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n - 1; i++)            // Line check
+            {
+                for (j = 1; j <= n - i; j++)        // Space before first star
+                {     
+                    Console.Write(" ");
+                }
+                    if (i > 1) Console.Write("* "); // Make sure there is no second star on the first line
+                for (j = 3; j <= i; j++)            // Space within the triangle 
+                {
+                    Console.Write("  ");
+                }
+                Console.Write("*");                 // The last (enclosing) star on every line
+                Console.WriteLine();                // Go to the next line
+            }
+            for (j = 1; j <= i; j++)                // Drawing the last line
+            {
+                Console.Write("* ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}  
+
---
+++ xAndy/xAndy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xAndy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            int y = 69;
+            for (int i = 0; i < 21; i = i + 4)
+            {
+                if (i % 2 == 0)
+                    x = y - x;
+                else
+                    x = y + 1;
+                y = y - 5;
+            }
+            Console.WriteLine("{0}, {1}", x, y);
+            Console.ReadKey();
+        }
+     }
+}
+
