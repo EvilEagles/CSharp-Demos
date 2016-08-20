1868d3f22ca59ea939122b09040b44d60af8ac13
Alex Vu <alex@igv.cafe>
2016-08-20 23:35:48 +0700

    Scrabble Word Calculator now Deliverable

--- ScrabbleWordCalculator/ScrabbleWordCalculator/Program.cs
+++ ScrabbleWordCalculator/ScrabbleWordCalculator/Program.cs
@@ -29,8 +29,10 @@ namespace ScrabbleWordCalculator
         #region
         public static bool letterX2 = false;
         public static bool wordX2 = false;
-        public static int returnedValue;
-        public static int score;
+        public static int letterScore;
+        public static int wordScore;
+        public static int totalScore;
+        public static string name;        
         #endregion
 
         // FUNCTION: GET THE SCORE FOR THE CHAR PARAMETER
@@ -39,72 +41,89 @@ namespace ScrabbleWordCalculator
             if (one.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(1x2) ");
                 return 2;
             }
             else if (one.Contains(letter))
+            {
+                Console.Write(letter + "(1) ");
                 return 1;
-
-
+            }
             else if (two.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(2x2) ");
                 return 4;
             }
             else if (two.Contains(letter))
+            {
+                Console.Write(letter + "(2) ");
                 return 2;
-
-
+            }
             else if (three.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(3x2) ");
                 return 6;
             }
             else if (three.Contains(letter))
+            {
+                Console.Write(letter + "(3) ");
                 return 3;
-
-
+            }                
             else if (four.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(4x2) ");
                 return 8;
             }
             else if (four.Contains(letter))
+            {
+                Console.Write(letter + "(4) ");
                 return 4;
-
-
+            }
+                
             else if (five.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(5x2) ");
                 return 10;
             }
             else if (five.Contains(letter))
+            {
+                Console.Write(letter + "(5) ");
                 return 5;
-
-
+            }                
             else if (eight.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(8x2) ");
                 return 16;
             }
             else if (eight.Contains(letter))
+            {
+                Console.Write(letter + "(8) ");
                 return 8;
-
-
+            }                
             else if (ten.Contains(letter) && letterX2 == true)
             {
                 letterX2 = false;
+                Console.Write(letter + "(10x2) ");
                 return 20;
             }
             else if (ten.Contains(letter))
+            {
+                Console.Write(letter + "(10) ");
                 return 10;
-
-
+            }                
             else
                 return 0;
         }
 
         static void Main(string[] args)
         {
+            Console.Write("Please input player name: ");
+            name = Console.ReadLine();
             while (true)
             {
                 Console.Write("Please input a valid English word: ");
@@ -123,18 +142,22 @@ namespace ScrabbleWordCalculator
                     }
                     else
                     {
-                        returnedValue = getScore(letter);
-                        score += returnedValue;
+                        letterScore = getScore(letter);
+                        wordScore += letterScore;
                     }
                 }
                 if (wordX2 == true)
                 {
-                    score += score;
+                    Console.WriteLine("   | Word x 2: YES");
+                    wordScore += wordScore;
                     wordX2 = false;
                 }
-                Console.WriteLine("The score is: " + score);
-                Console.WriteLine();
-                score = 0;
+                else
+                    Console.Write("   | Word x 2: NO");
+                totalScore += wordScore;
+                Console.WriteLine("Player name: {0}     | Word Score: {1}       | Total Score: {2}", name, wordScore, totalScore);           
+                Console.WriteLine();                
+                wordScore = 0;
             }
         }
     }
